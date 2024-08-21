ec7e4d061b40019e911c46d27a27ecc55010ac25
Joel Samson <joel.samson@hotmail.com>
2024-08-21 13:00:36 -0400

    Update Clear Entrust Cache.bat

--- Clear Entrust Cache.bat
+++ Clear Entrust Cache.bat
@@ -1,10 +1,29 @@
-@echo on
-cd "\program files (x86)\Common Files\Entrust\ESP"
-EESPUTIL -CLEARCACHE capolicy
-EESPUTIL -CLEARCACHE crl
-EESPUTIL -CLEARCACHE cacerts
-EESPUTIL -CLEARCACHE usercerts
-EESPUTIL -CLEARCACHE keyaccess
-EESPUTIL -CLEARCACHE logbinarydetails
-EESPUTIL -CLEARCACHE policycerts
+@echo off
+
+REM Check if ecautil.exe exists
+IF EXIST "C:\Program Files (x86)\Common Files\Entrust\ECAW\ecautil.exe" (
+    cd "C:\Program Files (x86)\Common Files\Entrust\ECAW"
+    ecautil.exe -CLEARCACHE capolicy
+    ecautil.exe -CLEARCACHE crl
+    ecautil.exe -CLEARCACHE cacerts
+    ecautil.exe -CLEARCACHE usercerts
+    ecautil.exe -CLEARCACHE keyaccess
+    ecautil.exe -CLEARCACHE logbinarydetails
+    ecautil.exe -CLEARCACHE policycerts
+) ELSE (
+    REM Check if EESPUTIL.exe exists
+    IF EXIST "C:\Program Files (x86)\Common Files\Entrust\ESP\EESPUTIL.exe" (
+        cd "C:\Program Files (x86)\Common Files\Entrust\ESP"
+        EESPUTIL.exe -CLEARCACHE capolicy
+        EESPUTIL.exe -CLEARCACHE crl
+        EESPUTIL.exe -CLEARCACHE cacerts
+        EESPUTIL.exe -CLEARCACHE usercerts
+        EESPUTIL.exe -CLEARCACHE keyaccess
+        EESPUTIL.exe -CLEARCACHE logbinarydetails
+        EESPUTIL.exe -CLEARCACHE policycerts
+    ) ELSE (
+        echo Neither ecautil.exe nor EESPUTIL.exe were found.
+    )
+)
+
 pause
